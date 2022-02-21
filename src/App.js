c26c32b4ecb2f2a5f7424e0b014b61db02a91d55
pgat3000 <pgat3000@gmail.com>
2022-02-21 16:25:09 -0600

    avance fake server

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import  Header from './Components/Header'
 import Tasks from './Components/Tasks';
-import { useState } from "react"
+import { useState , useEffect } from "react"
 import AddTask from './Components/AddTask';
 function App() {
   const [showAddTask, setShowAddTask]=useState
   (false)
 
-  const [tasks,setTasks] = useState(
-    [
-        {id: 1,
-        text: 'Doctors',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-        },
-        {id: 2,
-            text: 'Work',
-            day: 'Feb 21th at 3:30pm',
-            reminder: false,
-        },
-        {id: 3,
-            text: 'Shopping',
-            day: 'Feb 22th at 2:30pm',
-            reminder: true,
-            },
-        ]
-)
-
+  const [tasks,setTasks] = useState([])
+  useEffect(()=> {
+    const getTasks = async () =>{
+      const taskFromServer = await  fecthTasks()
+      setTasks(taskFromServer)
+    }
+    getTasks()
+  },[])
+//fetch
+const fecthTasks = async() =>{
+  const  res = await fetch(
+    'http://localhost:5000/tasks'
+  )
+  const data = await res.json()
+  return data
+}
 //Delete Task
 const deleteTask = (id) =>{
   setTasks(tasks.filter((task)=> task.id!==
